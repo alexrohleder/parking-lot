f98863a2a0172bd398c144456e3e9193a49a5be2
Alex Rohleder <a.rohleder@sportradar.com>
2020-10-17 21:03:18 +0200

    add Garage service with mocked values

---
+++ services/Garage.ts
@@ -0,0 +1,10 @@
+/**
+ * Retrieve the current amount earned today and a delta in the form of a
+ * percentage which represents the difference between today and yesterday
+ */
+export async function getTodayEarnings() {
+  return {
+    amount: 7580,
+    delta: 18
+  };
+}

531c35e9674fd20044451acfbf70b85d7ed932df
Alex Rohleder <a.rohleder@sportradar.com>
2020-10-17 21:01:26 +0200

    add AsyncReturnType helper to ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+// https://www.jpwilliams.dev/how-to-unpack-the-return-type-of-a-promise-in-typescript
+type AsyncReturnType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => Promise<infer U>
+  ? U
+  : T extends (...args: any) => infer U
+  ? U
+  : any;
